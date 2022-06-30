bf90007625ac19c92bf36217795475d67ffff721
Rebekah <rebekahshaw92@yahoo.co.uk>
2022-06-30 11:35:26 +0100

    main files added

--- README.md
+++ README.md
@@ -1,70 +1,65 @@
-# Getting Started with Create React App
+# Frontend Mentor - Tip calculator app solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-## Available Scripts
+## Table of contents
 
-In the project directory, you can run:
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
 
-### `yarn start`
+## Overview
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### The challenge
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Users should be able to:
 
-### `yarn test`
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Screenshot
 
-### `yarn build`
+#### Desktop
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![Desktop](images/desktop.png)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+#### Mobile
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+![Mobile](images/mobile.png)
 
-### `yarn eject`
+### Links
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+- Solution URL: [Solution](https://github.com/rebekahshaw92/react-tip-calculator-app)
+- Live Site URL: [Live Site](https://rebekahshaw92.github.io/react-tip-calculator-app/)
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## My Process
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+### Built with 
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+- Semantic HTML5 markup
+- CSS custom properties
+- Bootstrap for React
+- React JS
+- Mobile-first workflow
 
-## Learn More
+### What I Learned 
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+I decied that I would recreate this task using React as it had been a while since I had used React. This project was straight forward and allowed me to learn more about React.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+### Userful Resources 
 
-### Code Splitting
+- [Bootstrap]https://react-bootstrap.github.io) - This site helped me learn all I needed to know about Bootstrap for React.
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+## Author
 
-### Analyzing the Bundle Size
+- Website - [Rebekah Shaw](https://www.rebekahshaw.com)
+- Frontend Mentor - [@rebekahshaw92](https://www.frontendmentor.io/profile/rebekahshaw92)
+- Twitter - [@x_beckyboo_x](https://www.twitter.com/x_beckyboo_x)
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -1,90 +1,136 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 
-import TipDisplay from './TipDisplay';
+// import TipDisplay from './TipDisplay';
 
 const TipForm = () => {
 
-    const defaultState = {
-        bill: 0,
-        people: 0,
-        tip: 0,
+ const [bill, setBill] = useState('');
+ const [numberOfPeople, setNumberOfPeople] = useState ('');
+ const [totalAmount, setTotalAmount] = useState ('');
+ const [tipAmount, setTipAmount] = useState ('');
+ const [percentage, setPercentage] = useState ('');
+
+ const handleBill = (e) => {
+    if (e.target.value <= 0) {
+        setBill('');
+    } else {
+        setBill(e.target.value);
     }
+ }
 
-    const [state, setState] = useState(defaultState);
-    const [inputIsValid, setInputIsValid] = useState(true);
-    const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
-
-    const [tipPerPerson, setTipPerPerson] = useState(0);
-    const [totalPerPerson, setTotalPerPerson] = useState(0);
-
-    const tipPercentage = [5, 10, 15, 20, 50];
-
-    const handleInputChange = (e) => {
-        const inputValue = e.target.value;
-        const inputName = e.target.name;
-    
-
-    if (inputName === 'people' && inputValue === 0) {
-        setInputIsValid(false);
-    } else if (inputName === 'people' && inputValue !== 0) {
-        setInputIsValid(true);
+ const handleNumberOfPeople = (e) => {
+    if (e.target.value <= 0){
+        setNumberOfPeople('');
+    } else {
+        setNumberOfPeople(e.target.value);
     }
+ }
 
+ const handlePercent = (e) => {
+    if (e.target.value <= 0){
+        setPercentage('');
+    } else {
+        setPercentage(e.target.value);
     }
+ }
+
+ const handleButton = (e) => {
+    e.preventDefault();
+    setPercentage('');
+    setPercentage(e.target.value);
+ }
+
+ const handleReset = (e) => {
+    e.preventDefault();
+    setBill('');
+    setNumberOfPeople('');
+    setPercentage('');
+    setTipAmount('');
+    setTotalAmount('');
+  }
+
+ useEffect(() => {
+    if (bill > 0 && numberOfPeople > 0 && percentage > 0) {
+        const totalTip = +bill * (+percentage / 100);
+        const totalBill = +bill + +totalTip;
+        setTipAmount((+totalTip / +numberOfPeople).toFixed(2));
+        setTotalAmount((+totalBill / +numberOfPeople).toFixed(2));
+    } else {
+        setTipAmount('0.00');
+        setTotalAmount('0.00');
+    }
+ }, [bill, numberOfPeople, percentage]);
+
         return (
-            <Row>
-            <Col lg={6}>
                 <Row>
-                    <Col xs={7} lg={8}>
-                        <h5 className="billTitle">Bill</h5>
-                    </Col>
-                    <Col xs={5} lg={4}>
-                        <p className="errorBill"></p>
-                    </Col>
-                </Row>
-                <Form.Group className="billInput mb-2">
-                    <Form.Control type="text" className="bill form-control" placeholder="0.00" aria-label="0.00" aria-describedby="dollarIcon" />
-                </Form.Group>
-                <h5 className="tipTitle">Select Tip %</h5>
-                <Row gap={0} className="d-flex">
-                    <Col xs={5} lg={4}>
-                        <Button className="btn percentage" value="5">5%</Button>
-                    </Col>
+                    <Col lg={6}>
+                        <Row>
+                            <Col xs={7} lg={8}>
+                                <h5 className="billTitle">Bill</h5>
+                            </Col>
+                        </Row>
+                        <Form.Group className="billInput mb-2">
+                            <Form.Control type="text" onChange={handleBill} value={bill} className="bill form-control" placeholder="0.00" aria-label="0.00" aria-describedby="dollarIcon" />
+                        </Form.Group>
+                        <h5 className="tipTitle">Select Tip %</h5>
+                        <Row gap={0} className="d-flex">
+                            <Col xs={5} lg={4}>
+                                <Button className="btn percentage" onClick={handleButton} value={5}>5%</Button>
+                            </Col>
 
-                    <Col xs={5} lg={4}>
-                        <Button className="btn percentage" value="10">10%</Button>
-                    </Col>
-
-                    <Col xs={5} lg={4}>
-                        <Button className="btn percentage" value="15">15%</Button>
+                            <Col xs={5} lg={4}>
+                        <Button className="btn percentage" onClick={handleButton} value={10}>10%</Button>
                     </Col>
                     <Col xs={5} lg={4}>
-                        <Button className="btn percentage" value="25">25%</Button>
+                        <Button className="btn percentage" onClick={handleButton} value={15}>15%</Button>
                     </Col>
                     <Col xs={5} lg={4}>
-                        <Button className="btn percentage" value="50">50%</Button>
+                        <Button className="btn percentage" onClick={handleButton} value={25}>25%</Button>
+                    </Col>
+                    <Col xs={5} lg={4}>
+                        <Button className="btn percentage" onClick={handleButton} value={50}>50%</Button>
                     </Col>
                     <Col xs={4}>
-                    <Form.Control type="text" className="custom form-control" placeholder="Custom" />
+                        <Form.Control type="text" className="custom form-control" value={percentage} onChange={handlePercent} placeholder="Custom" />
                     </Col>
                 </Row>
                 <Row>
                     <Col lg={8}>
                         <h5 className="numberTitle">Number of People</h5>
                     </Col>
-                    <Col lg={4}>
-                        <p className="errorPeople"></p>
-                    </Col>
                 </Row>
-
                 <Form.Group className="peopleInput mb-2">
-                <Form.Control type="text" className="people form-control" placeholder="5" aria-label="5" aria-describedby="dollarIcon" /> 
+                    <Form.Control type="text" value={numberOfPeople} onChange={handleNumberOfPeople} className="people form-control" placeholder="5" aria-label="5" aria-describedby="dollarIcon" />
                 </Form.Group>
+            </Col> 
+            {/* <TipDisplay /> */}
+
+            <Col lg={6}>
+                <div className="tipAmount">
+                    <Row>
+                        <Col xs={8}>
+                            <h6 className="tipAmountTitle">Tip Amount</h6>
+                            <h7 className="person">/ person</h7>
+                        </Col>
+                        <Col xs={4}>
+                            <h2 className="amount">${tipAmount}</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={8}>
+                        <h6 className="totalTitle">Total</h6>
+                        <h7 className="person">/ person</h7>
+                        </Col>
+                        <Col xs={4}>
+                        <h2 className="amount totalAmount">${totalAmount}</h2>
+                        </Col>
+                    </Row>
+                   <Button onSubmit={handleReset} className="btn reset">Reset</Button>
+                    </div>
             </Col>
-            <TipDisplay />
     </Row>
-        )
+        );
     }
 
 export default TipForm;
